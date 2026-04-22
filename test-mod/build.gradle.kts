cfd7e2a6d3ae26cc3afc11e510c53fd6c76c56e9
ImBit <151650938+ImBit@users.noreply.github.com>
2026-04-22 23:20:44 +0100

    🐛 Fix test mod.

--- test-mod/build.gradle.kts
+++ test-mod/build.gradle.kts
@@ -7,5 +7,7 @@ loom.runConfigs["client"].ideConfigGenerated(true)
 dependencies {
     implementation(libs.fabric.api)
     implementation(project(":api"))
+    include(project(":api"))
     implementation(project(":coroutines"))
+    include(project(":coroutines"))
 }
